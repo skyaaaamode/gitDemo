0fe527758fe8ac9818c73fdcf442dd5dd1eb5fb5
zzf <zzf@375874825>
2020-09-28 22:47:07 +0800

    mster分支

---
+++ src/main/java/MyDemo.java
@@ -0,0 +1,11 @@
+/**
+ * @author 37587
+ * @version 1.0
+ * @date 2020/9/28 22:45
+ * @description
+ */
+public class MyDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
